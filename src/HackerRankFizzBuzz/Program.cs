5999fa5ea19bab570464f7293cc9d90065014431
ErickMaia <erickmaiatmariano@gmail.com>
2021-11-08 05:22:26 -0300

    HackerRankFizzBuzz

---
+++ src/HackerRankFizzBuzz/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace HackerRankFizzBuzz
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            Result.fizzBuzz(n);
+        }
+    }
+
+    class Result
+    {
+        public static void fizzBuzz(int n)
+        {
+
+            if(n<1) return; 
+
+            for(int i=1; i<=n; i++)
+            {
+                string result = "";
+            
+                if(i%3==0)
+                {
+                    result += "Fizz"; 
+                }
+
+                if(i%5==0)
+                {
+                    result += "Buzz"; 
+                }
+
+                if(result == "")
+                {
+                    result = i.ToString(); 
+                }
+
+                Console.WriteLine(result); 
+            }
+
+            
+        }
+    }
+}
